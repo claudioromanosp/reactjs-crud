9856317afabe18b545654dc7ae78b51b6feb09e8
¨Claudio <claudio.romano@jolivi.com.br>
2020-01-20 14:14:40 -0300

    first commit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+
+<Main icon="home"
+		title="inicio"
+		subtitle="projeto feito com react">
+		<div className="display-4">Bem-vindo!</div>
+		<hr />
+		<p className="mb-0">Sistema para criação de formulários</p>
+</Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,7 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+	<footer className='footer'>
+		<p>Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Claudio<span className="text-danger">Romano</span></strong></p>
+	</footer>
